51ec270224100e3b05366b2feb37a28596c8cd57
bigbeno37 <contact@benosullivan.com>
2022-05-09 19:44:37 +1000

    Added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-      "./index.html",
-      "./src/**/*.{vue,ts}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./index.html',
+		'./src/**/*.{vue,ts}'
+	],
+	theme: {
+		extend: {},
+	},
+	// eslint-disable-next-line no-undef
+	plugins: [require('daisyui')],
+};
