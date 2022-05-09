61e00ef0b71dc92efd84352f5e9d48486d8df887
bigbeno37 <contact@benosullivan.com>
2022-05-09 20:02:46 +1000

    Updated look of Quiz component

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -4,7 +4,14 @@ let foo = 'far';
 
 <template>
 	<div>
-		<h1>What is 5<sub>8</sub> in base 2?</h1>
-		<input type="text" class="input input-bordered" />
+		<h1 class="text-center text-6xl">What is 5<sub>8</sub> in base 2?</h1>
+		<input type="text" class="input input-bordered input-lg w-full mt-12 mb-8" />
+		<button type="button" class="btn btn-accent w-full">Submit</button>
 	</div>
 </template>
+
+<style scoped>
+    h1 {
+        font-family: 'Source Code Pro', ui-sans-serif;
+    }
+</style>
