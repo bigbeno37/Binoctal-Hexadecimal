2fc02de8f461b59d74c561059d255e5f9d2710c9
bigbeno37 <contact@benosullivan.com>
2022-05-09 19:13:12 +1000

    Starting from scratch with Vite + Vue

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': 'vue-eslint-parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'parser': '@typescript-eslint/parser',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'vue',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'vue/no-multiple-template-root': 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/script-indent': ['error', 'tab']
+	}
+};
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<h1 class="text-2xl text-red-600">Hello world!</h1>
+</template>
