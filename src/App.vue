c04edbc5a02d13e4d32ff192b26cf301e84acc7b
bigbeno37 <contact@benosullivan.com>
2022-05-09 19:44:56 +1000

    Added main menu; Added basic quiz template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+import Quiz from './Quiz.vue';
+
+const difficulty = ref<'easy' | 'medium' | 'hard' | null>(null);
 </script>
 
 <template>
-	<h1 class="text-2xl text-red-600">Hello world!</h1>
+
+	<div class="w-full h-full flex flex-row justify-center">
+		<div class="w-[900px] self-center">
+			<Quiz v-if="difficulty !== null" />
+			<div v-else class="flex flex-col">
+				<h1 class="text-8xl text-center mb-8">Binoctal Hexadecimal</h1>
+				<button class="btn btn-primary btn-lg self-center w-60" @click="difficulty = 'easy'">Easy</button>
+				<button class="btn btn-secondary btn-lg self-center w-60 my-8" @click="difficulty = 'medium'">Medium</button>
+				<button class="btn btn-accent btn-lg self-center w-60" @click="difficulty = 'hard'">Hard</button>
+			</div>
+		</div>
+	</div>
 </template>
+
+<style scoped>
+	h1 {
+		font-family: 'Source Code Pro', ui-sans-serif;
+	}
+</style>
---
+++ src/Quiz.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+let foo = 'far';
+</script>
+
+<template>
+	<div>
+		<h1>What is 5<sub>8</sub> in base 2?</h1>
+		<input type="text" class="input input-bordered" />
+	</div>
+</template>
