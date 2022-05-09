54c5a229b26ea3fb1c71b48439401c5c963fd1d6
bigbeno37 <contact@benosullivan.com>
2022-05-09 19:53:12 +1000

    Extracted main menu into separate component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import {ref} from 'vue';
 import Quiz from './Quiz.vue';
+import MainMenu from './MainMenu.vue';
 
 const difficulty = ref<'easy' | 'medium' | 'hard' | null>(null);
+
+const onDifficultyClicked = (newDifficulty: 'easy' | 'medium' | 'hard') => {
+	difficulty.value = newDifficulty;
+};
 </script>
 
 <template>
@@ -10,12 +15,7 @@ const difficulty = ref<'easy' | 'medium' | 'hard' | null>(null);
 	<div class="w-full h-full flex flex-row justify-center">
 		<div class="w-[900px] self-center">
 			<Quiz v-if="difficulty !== null" />
-			<div v-else class="flex flex-col">
-				<h1 class="text-8xl text-center mb-8">Binoctal Hexadecimal</h1>
-				<button class="btn btn-primary btn-lg self-center w-60" @click="difficulty = 'easy'">Easy</button>
-				<button class="btn btn-secondary btn-lg self-center w-60 my-8" @click="difficulty = 'medium'">Medium</button>
-				<button class="btn btn-accent btn-lg self-center w-60" @click="difficulty = 'hard'">Hard</button>
-			</div>
+			<MainMenu v-else :on-difficulty-clicked="onDifficultyClicked"/>
 		</div>
 	</div>
 </template>
---
+++ src/MainMenu.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{ onDifficultyClicked: (difficulty: 'easy' | 'medium' | 'hard') => void }>();
+</script>
+
+<template>
+	<div class="flex flex-col">
+		<h1 class="text-8xl text-center mb-8">Binoctal Hexadecimal</h1>
+		<button class="btn btn-primary btn-lg self-center w-60" @click="props.onDifficultyClicked('easy')">Easy</button>
+		<button class="btn btn-secondary btn-lg self-center w-60 my-8" @click="props.onDifficultyClicked('medium')">Medium</button>
+		<button class="btn btn-accent btn-lg self-center w-60" @click="props.onDifficultyClicked('hard')">Hard</button>
+	</div>
+</template>
